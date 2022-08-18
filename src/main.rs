ce666abb248086f03a6cc409c49f19133957c40e
Richard McDonald <rmcdonald@wisesystems.com>
2022-08-18 15:35:17 -0400

    cleaner

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,9 @@
-use chrono::serde::ts_seconds;
+use chrono::serde::{ts_seconds, ts_seconds_option};
 use chrono::{DateTime, NaiveDateTime, Utc};
 use serde::{de, Deserialize, Deserializer, Serialize, Serializer};
 use std::io;
 use std::process::Command;
 use std::str;
-
-// fn date_time_from_str<'de, D>(deserializer: D) -> Result<DateTime<Utc>, D::Error>
-// where
-//     D: Deserializer<'de>,
-// {
-//     let i: i64 = Deserialize::deserialize(deserializer)?;
-//     let s: String = i.to_string();
-//     // NaiveDateTime::parse_from_str(&s, "%Y-%m-%dT%H:%M:%S.%f").map_err(de::Error::custom)
-//     let a = NaiveDateTime::from_timestamp(i, 0);
-//     Ok(a)
-// }
-
-// fn naive_date_time_from_str<'de, D>(deserializer: D) -> Result<NaiveDateTime, D::Error>
-// where
-//     D: Deserializer<'de>,
-// {
-//     let i: i64 = Deserialize::deserialize(deserializer)?;
-//     let s: String = i.to_string();
-//     // NaiveDateTime::parse_from_str(&s, "%Y-%m-%dT%H:%M:%S.%f").map_err(de::Error::custom)
-//     let a = NaiveDateTime::from_timestamp(i, 0);
-//     Ok(a)
-// }
-
-// fn naive_date_time_to_str<S: Serializer>(
-//     time: &NaiveDateTime,
-//     serializer: S,
-// ) -> Result<S::Ok, S::Error> {
-//     // time_to_json(time.clone()).serialize(serializer)
-//     DateTime::<Utc>::from_utc(time.clone(), Utc)
-//         .to_rfc3339()
-//         .serialize(serializer)
-// }
 
 #[derive(Deserialize, Serialize, Debug)]
 #[serde(rename_all = "camelCase")]
@@ -43,18 +11,28 @@ struct Job {
     job_arn: String,
     job_id: String,
     job_name: String,
-    // #[serde(deserialize_with = "date_time_from_str")]
-    // #[serde(serialize_with = "naive_date_time_to_str")]
     #[serde(with = "ts_seconds")]
     created_at: DateTime<Utc>,
-    started_at: Option<u64>,
-    stopped_at: Option<u64>,
+    #[serde(default)]
+    #[serde(with = "ts_seconds")]
+    started_at: DateTime<Utc>,
+    #[serde(default)]
+    #[serde(with = "ts_seconds")]
+    stopped_at: DateTime<Utc>,
 }
 
 #[derive(Deserialize, Serialize, Debug)]
 #[serde(rename_all = "camelCase")]
 struct JobSummaryList {
     job_summary_list: Vec<Job>,
+}
+
+fn write_stdout<'a, T>(val: &'a T)
+where
+    T: Serialize,
+{
+    let results_file = io::stdout();
+    serde_json::to_writer_pretty(&results_file, val).unwrap();
 }
 
 fn main() {
@@ -78,10 +56,8 @@ fn main() {
     let jobs: Vec<&Job> = job_summary_list
         .job_summary_list
         .iter()
-        .filter(|job| job.started_at.is_some())
+        .filter(|job| job.started_at.timestamp() != 0)
         .collect();
 
-    let results_file = io::stdout();
-    // serde_json::to_writer_pretty(&results_file, &job_summary_list).unwrap();
-    serde_json::to_writer_pretty(&results_file, &jobs).unwrap();
+    write_stdout(&jobs);
 }
